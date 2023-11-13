e8e04a1ebcd918fca9144f450c1f67ffb4c334ad
Pasha-lt <9139136@gmail.com>
2023-11-13 01:41:06 -0500

    gitignore and expand l4

---
+++ lesson_4/del.py
@@ -0,0 +1,5 @@
+del_product = 4
+list_products = [3,4,5,6]
+print(list_products)
+list_products.remove(5)
+print(list_products)
---
+++ lesson_4/lesson_4.py
@@ -0,0 +1,65 @@
+# print("fid"[::-1])
+#
+#
+# lis2 = ["dsfd", "fsdfds", "fdsf"]
+# print(lis2)
+#
+# lis2.pop(1)
+# print(lis2)
+#
+# # приклад з домашки
+# import copy
+# product_list = [32,43,43]
+# product_list_2 = copy.deepcopy(product_list)
+# add_product = "few"
+# product_list_2.append(add_product)
+# print(f"Your purchase was updated: {product_list_2.append(add_product)}")
+#
+
+#
+# list_a = [3,4]
+# list_b = [7,4]
+# print(id(list_a))
+# print(id(list_b))
+# list_a.extend(list_b)
+# print(list_a)
+# # a = (3,4,4)
+# # print(a[0])
+#
+# list_products = [3,4,5,6]
+# print(list_products)
+# list_products.remove(5)
+# print(list_products)
+
+
+# import math
+# print(math.ceil(0.0001))
+# print(math.floor(1.999))
+# print(math.ceil(0.0001) == math.floor(1.999))
+
+# КОНСТАНТИ
+
+
+# a = "fdsf 2 dsfds 2 fdsf 2 fff"
+# b = a.split("2")
+# print(b)
+#
+#
+
+
+#
+# # for i in "Робіть домашку":
+# #     print(i)
+#
+#
+# new_string = ";".join("fds")
+# print(list_b)
+list_a = ["12", "32", "32"]
+string_from_list = "".join(list_a)
+print(string_from_list)
+
+#
+#
+# # linkedin рекрутери. Пододавайте один одного в друзі.
+
+# формат і ф стрінг
